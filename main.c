5de7e4e85229bd57727a3783d0d926e9eb3f72fb
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-11-20 12:09:37 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "fonctions.h"
+
+// test des tcf A et B
+int main()
+{
+
+    partA("input.txt");
+
+    int a ;
+    a = partB("input.txt");
+    printf("%d\n", a) ;
+
+    /*test transfo ascii
+    char codebin[] = "00000000";
+    transfo_Ascii('a', codebin);
+    printf("%s", codebin);*/
+
+    return 0;
+}
+
+
+/*Partie 1, A
+transformer un fichier Alice.txt avec des mots en fichier Output.txt avec des 0 ou 1
+pour cela : lire le fichier Alice.txt
+prendre les caractères un par un
+
+transformer un caractère en code binaire
+->avoir le code décimal
+->le transformer en binaire
+
+mettre la correspondance binaire du caratère dans un autre fichier (crée si non existant)
+*/
+
+/*Partie 1, B
+afficher le nb de caractères dans un fichier .txt
+normalement : alice.txt *8 = output.txt
+*/
