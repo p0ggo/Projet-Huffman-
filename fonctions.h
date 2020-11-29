e40dd0d1f8671bd15a2179589219645b29ebbebe
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-11-29 18:07:33 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -7,3 +7,10 @@ typedef struct Element{
 int search_char(char letter, Element* list);
 
 Element* partC(char *filename);
+
+typedef struct Node {
+    char character;
+    int occ;
+    struct Node* gauche ;
+    struct Node* droite ;
+}Node;
