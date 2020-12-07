2fe65a5f1cb6e9acfb4b2beaf56629568f20f492
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-07 17:12:13 +0100

    Create part1_fct.c

---
+++ part1_fct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "part1_fonctions.h"
+
+
+void transfo_bin(char *filename){
+  return 0; 
+}
+
+
+int count_char(char *filename){
+    return 0;
+}
