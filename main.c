5134732f1d12bb8ed014f313d86156032d77bc01
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-07 17:50:20 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "part1_fonctions.h"
+#include "part2_fonctions.h"
+#include "part3_fonctions.h"
+
+
+int main (void) {
+
+  printf("hello"); 
+  return 0; 
+
+}
