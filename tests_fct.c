ef18d240928588bbc13e70f5097e06eb7af29321
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-09 23:14:10 +0100

    Update tests_fct.c

--- tests_fct.c
+++ tests_fct.c
@@ -1,1 +1,36 @@
 
+void afficherListe (Element * liste){
+
+    Element * temp ;
+    temp = liste ;
+    while (temp != NULL){
+        printf("%c, ", temp->character);
+        printf("%d \n", temp->occ) ;
+        temp = temp->next ;
+    }
+}
+
+void test_fct(){
+  // test fct A
+  transfo_bin("input.txt") ;
+  //ouvrir le fichier ouput pour vérifier qu'il y a bien des 0 et des 1 dedans 
+  
+  //test fct B
+  int a ;
+  printf("debut\n");
+  a = count_char("input.txt");
+  printf("%d\n", a) ;
+
+  // test fct C 
+  Element* maliste = NULL ;
+  maliste= create_list_from_txt("input.txt");
+  afficherListe (maliste);
+  free(maliste);
+  
+  
+  
+  
+}
+
+
+
