0d4d4956194ca0bf34153a3232492df04fc59c43
p0ggo <60432111+p0ggo@users.noreply.github.com>
2020-12-10 19:35:34 +0100

    Update read_txt.h

--- Read/read_txt.h
+++ Read/read_txt.h
@@ -1,12 +1,29 @@
-//
-// Created by Chloé  Rolland  on 07/12/2020.
-//
-
+/**
+ * @file read_txt.h
+ * @author test
+ * @brief  function to read various text
+ * @version 0.1
+ * @date 2020-12-10
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef PJ_HUFFMAN_READ_TXT_H
 #define PJ_HUFFMAN_READ_TXT_H
 #include "struct.h"
-
+/**
+ * @brief 
+ * 
+ * @param exit 
+ */
 void read_txt(char *exit);
+/**
+ * @brief 
+ * 
+ * @param txt 
+ * @param listChar 
+ * @return Letter* 
+ */
 Letter * r_s_dico (char*txt, List_char*listChar);
 
 #endif //PJ_HUFFMAN_READ_TXT_H
