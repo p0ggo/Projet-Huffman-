8de2ab6b62676a4ab9a295f13d4f694147bfdf67
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 22:09:46 +0100

    Update part3_fct.c

--- part3/part3_fct.c
+++ part3/part3_fct.c
@@ -2,3 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 #include "part3_fonctions.h"
+
+typedef struct Node {
+    char chara;
+    int occur;
+    struct Node *right;
+    struct Node *left;
+} Node;
+
+void dicho_search ( Node tab_noeud [], char letter ){
+  
+}
+
+void sort_tab_node ( Node tab_noeud []) {
+  
+}
+
+Node* treehuffman (Node tab_noeud []){
+  
+}
+
+void transform_into_avl (Node* treehuffman ){
+  
+}
+
+void compress (char* filename){
+  
+}
+
+void decompress (char* filename){
+  
+}
+
