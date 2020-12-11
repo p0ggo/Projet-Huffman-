6f804c05f3a1e317ea46fe827b0450bc3e3c69c3
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 22:06:00 +0100

    Update part2_fct.c

--- part2/part2_fct.c
+++ part2/part2_fct.c
@@ -3,6 +3,19 @@
 #include <math.h>
 #include "part2_fonctions.h"
 
+typedef struct Element{
+    char character;
+    int occ;
+    struct Element *next;
+}Element;
+
+typedef struct Node {
+    char chara;
+    int occur;
+    struct Node *right;
+    struct Node *left;
+} Node;
+
 Element* create_list_from_txt(char *filename){
       return NULL ; 
 }
