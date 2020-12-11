6880a0ff87a52ff8cb654ec22745a5a04566d023
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 19:30:02 +0100

    Update part3_fonctions.h

--- part3/part3_fonctions.h
+++ part3/part3_fonctions.h
@@ -1,1 +1,11 @@
+void dicho_search ( Node tab_noeud [], char letter ) ; 
 
+void sort_tab_node ( Node tab_noeud []) ; 
+
+Node* treehuffman (Node tab_noeud []); 
+
+void transform_into_avl (Node* treehuffman ); 
+
+void compress (char* filename); 
+
+void decompress (char* filename); 
