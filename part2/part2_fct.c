0aa8a7fa74d216c5fe29cc07ff50621597342f40
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 18:18:47 +0100

    Update part2_fct.c

--- part2/part2_fct.c
+++ part2/part2_fct.c
@@ -3,3 +3,20 @@
 #include <math.h>
 #include "part2_fonctions.h"
 
+Element* create_list_from_txt(char *filename){
+      return NULL ; 
+}
+
+Node* huffman_tree(Element* Lsc){
+  return NULL ; 
+  
+}
+
+void write_dico(Node* huffmantree){
+  
+} 
+
+void w_huffman_code (char* namefile){
+  
+}
+
