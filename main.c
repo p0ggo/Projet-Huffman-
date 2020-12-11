47fbf2c67b0201f6548755c743b67d8bad65e178
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 16:14:51 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include "read_txt.h"
+#include "write_txt.h"
+#include "struct.h"
+#include "random.h"
+#include "tree.h"
+
+#define CHARMAX 1000
+
+
+int main (){
+
+
+
+} 
