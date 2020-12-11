61fb542da049bfe0d30f42c8e7ea89c87f2df717
p0ggo <60432111+p0ggo@users.noreply.github.com>
2020-12-11 18:06:47 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,5 +1,20 @@
+/**
+ * @brief transform a character 
+ * 
+ * @param letter 
+ * @param code_bin 
+ */
 void transfo_Ascii(char letter, char* code_bin);
-
+/**
+ * @brief transform into a text file that has  binary txt
+ * 
+ * @param filename 
+ */
 void transfo_bin(char *filename); 
-
+/**
+ * @brief count the number of character in a text file
+ * 
+ * @param filename 
+ * @return int 
+ */
 int count_char(char *filename); 
