d4f1b59d3ee3e24eb61af52455eda8cf224712b4
p0ggo <60432111+p0ggo@users.noreply.github.com>
2020-12-11 18:07:12 +0100

    Update fctC.h

--- fctC.h
+++ fctC.h
@@ -3,7 +3,18 @@ typedef struct Element{
     int occ;
     struct Element *next;
 }Element;
-
+/**
+ * @brief function that looks if a character is already in the list, return 1 if the element is in the list and 0 otherwise
+ * 
+ * @param letter 
+ * @param liste 
+ * @return Element* 
+ */
 Element* search_char(char letter, Element* liste);
-
+/**
+ * @brief function that returns a list of each and every character in the text, and the number of occurence of those characters
+ * 
+ * @param filename 
+ * @return Element* 
+ */
 Element* create_list_from_txt(char *filename);
