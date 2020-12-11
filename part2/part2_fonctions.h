67cde27ec488a3e0470fcd41f9940fc1ca87dd6c
joséphine de Lestrange <73129498+josephine-dl@users.noreply.github.com>
2020-12-11 18:22:20 +0100

    Update part2_fonctions.h

--- part2/part2_fonctions.h
+++ part2/part2_fonctions.h
@@ -1,1 +1,10 @@
 
+Element* create_list_from_txt(char *filename); 
+
+Node* huffman_tree(Element* Lsc); 
+
+void write_dico(Node* huffmantree); 
+
+void w_huffman_code (char* namefile); 
+
+void compress_txt(); 
